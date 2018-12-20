680889169f1d7fe16672ab23ad3baff5a9a97c93
rubinoAM <parkcathedralgrace@gmail.com>
2018-12-20 14:26:42 -0500

    Weather Graph added

--- script.js
+++ script.js
@@ -12,9 +12,9 @@ $(document).ready(function(){
             console.log(weatherData);
             const currTemp = weatherData.main.temp;
             const temps = {
-                curr: weatherData.main.temp,
-                max: weatherData.main.temp_max,
-                min: weatherData.main.temp_min
+                curr: Math.round(weatherData.main.temp),
+                max: Math.round(weatherData.main.temp_max),
+                min: Math.round(weatherData.main.temp_min),
             }
             const newHTML = `
                 <div>
@@ -23,6 +23,36 @@ $(document).ready(function(){
                 </div>
             `;
             $('.weather-data').html(newHTML);
+            animateTemp(0,currTemp);
         })
     })
 })
+
+const canvas = document.getElementById('canvas');
+let currentPercent = 0;
+let context = canvas.getContext('2d');
+console.log(canvas);
+
+function animateTemp(currentArc,currentTemp){
+    context.lineWidth = 5;
+    context.beginPath();
+    context.arc(155,85,70,0,Math.PI*2);
+    context.fillStyle = "#ccc";
+    context.closePath();
+    context.fill();
+
+    let fifthArg = (Math.PI*2*currentArc + Math.PI * 1.5); //This is just the 5th argument for line 49. I just wanted to keep the actual code from going to far to the right.
+    
+    context.beginPath();
+    context.arc(155,85,75,Math.PI*1.5,fifthArg); // Math.PI * 1.5 comes out to 12:00 on a circle
+    context.stroke();
+
+    //Update the arc % until we hit currentTemp
+    currentPercent++;
+    if(currentPercent < currentTemp){
+        //requestAnimationFrame is kind of like a while loop
+        requestAnimationFrame(()=>{
+            animateTemp(currentPercent/100,currentTemp)
+        })
+    }
+}
